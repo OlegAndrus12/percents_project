2ea7caac5408b4c5cb811257e942ba947b3577b0
OlegAndrus12 <andrusoleg123@gmail.com>
2021-10-26 11:27:32 +0300

    tasks view added and navbar is created, some more urls

--- app.py
+++ app.py
@@ -9,20 +9,31 @@ with open("data.json", "r") as f:
 
 
 @app.route('/')
+@app.route('/index')
 def index():
     context = {"text" : "BlaBlaCar", "ids" : [i for i in data]
     }
-    
     return render_template("welcome.html", **context)
 
 @app.route('/all')
 def all():
-    return render_template("tasks.html", data = data)
+    tasks = list()
+    for i in data.values():
+        for j in i:
+            tasks.append(tuple(j.values()))
+    return render_template("tasks.html", tasks = tasks)
 
 @app.route('/tasks/<task_id>')
 def tasks(task_id):
-    return render_template("tasks.html", data = (task_id, data[task_id]))
+    tasks = list()
+    for i in data[task_id]:
+        tasks.append(tuple(i.values()))
+    print(tasks)
+    return render_template("tasks.html", tasks = tasks) 
 
+@app.route('/details/<task_id>')
+def details(task_id):
+    return render_template("details.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
